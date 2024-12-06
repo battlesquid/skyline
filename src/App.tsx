4cef7b747ee15ee68bb9095cb039bef86f509598
Mayowa Esan <mayowa.esan@garmin.com>
2024-12-06 16:29:38 -0700

    refactor: tweak layout, camera

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { AppShell, Burger, Button, Group, LoadingOverlay, TextInput, useMantineTheme } from "@mantine/core";
+import { AppShell, Burger, Button, Divider, Group, LoadingOverlay, NumberInput, Space, Stack, TextInput, Tooltip, useMantineTheme } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { useRef, useState } from "react";
 import { useQuery } from "urql";
@@ -14,7 +14,7 @@ export default function App() {
   const [year, setYear] = useState(new Date().getFullYear());
 
   const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
-  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure();
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
 
   const [result] = useQuery({
     query: ContributionQuery,
@@ -25,13 +25,14 @@ export default function App() {
     },
   });
 
-  const input = useRef<HTMLInputElement>(null!);
+  const usernameRef = useRef<HTMLInputElement>(null!);
+  const yearRef = useRef<HTMLInputElement>(null!);
 
   const theme = useMantineTheme();
 
   return (
     <AppShell
-      header={{ height: 60 }}
+      header={{ height: 0 }}
       padding={"md"}
       navbar={{
         width: 300,
@@ -39,16 +40,32 @@ export default function App() {
         collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
       }}
     >
-      <AppShell.Header>
-        <Group h="100%" px="md">
-          <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
-          <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
-          skyline
-          <TextInput ref={input} placeholder="Github Username" />
-          <Button onClick={() => setName(input.current.value.trim())}>Search</Button>
-        </Group>
-      </AppShell.Header>
-      <AppShell.Main style={{ height: "calc(100vh - 96px)", backgroundColor: theme.colors.dark[7] }}>
+      <AppShell.Navbar p="md">
+        <AppShell.Section h="100%">
+          <Stack gap={5}>
+            <h2>skyline</h2>
+            {/* <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
+            <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" /> */}
+            <TextInput ref={usernameRef} label="Github Username" placeholder="Github Username"  />
+            <NumberInput ref={yearRef} min={2015} max={new Date().getFullYear()} label="Year" placeholder="Year"  />
+            <Button
+              fullWidth
+              onClick={() => {
+                setName(usernameRef.current.value.trim());
+                setYear(parseInt(yearRef.current.value));
+              }}>
+              Generate
+            </Button>
+          </Stack>
+        </AppShell.Section>
+        <Divider mb={10} />
+        <AppShell.Section>
+          <Tooltip label="Work In Progress">
+            <Button fullWidth>Export</Button>
+          </Tooltip>
+        </AppShell.Section>
+      </AppShell.Navbar>
+      <AppShell.Main style={{ height: "calc(100vh)", backgroundColor: theme.colors.dark[7] }}>
         <LoadingOverlay visible={result.fetching} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} />
         {!result.fetching && <Skyline weeks={result.data!.user!.contributionsCollection.contributionCalendar.weeks} />}
         <div style={{ position: "absolute", left: 0, right: 0, top: 0, bottom: 0, pointerEvents: "none" }}>
--- src/components/skyline.tsx
+++ src/components/skyline.tsx
@@ -1,9 +1,10 @@
-import { Billboard, CameraControls, Text } from "@react-three/drei";
+import { useMantineTheme } from "@mantine/core";
+import { CameraControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { ResultOf } from "gql.tada";
 import { ContributionQuery } from "../api/query";
 import { ContributionTower } from "./contribution_tower";
-import { useMantineTheme } from "@mantine/core";
+import { PerspectiveCamera } from "three";
 
 interface SkylineProps {
   weeks: NonNullable<ResultOf<typeof ContributionQuery>["user"]>["contributionsCollection"]["contributionCalendar"]["weeks"];
@@ -13,20 +14,16 @@ export function Skyline(props: SkylineProps) {
   const { weeks } = props;
   const theme = useMantineTheme();
 
+  const camera = new PerspectiveCamera(50, 1, 0.1, 1000)
+  camera.position.y = 20
+  camera.position.z = 30
+
   return (
-    <Canvas style={{ backgroundColor: theme.colors.dark[8] }}>
+    <Canvas camera={camera} style={{ backgroundColor: theme.colors.dark[8] }}>
       <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <spotLight position={[10, 60, 10]} angle={0.25} penumbra={1} decay={0} intensity={Math.PI / 2} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <CameraControls />
-      <Billboard
-        follow={true}
-        lockX={false}
-        lockY={false}
-        lockZ={false} // Lock the rotation on the z axis (default=false)
-      >
-        <Text fontSize={1}>I'm a billboard</Text>
-      </Billboard>
+      <CameraControls camera={camera}/>
       {weeks.map((week, i) =>
         week.contributionDays.map((day, j) => (
           <ContributionTower key={day.date.toString()} day={day} x={i - 26} y={j - 3.5} height={day.contributionCount * 0.2 + 0.2} />
