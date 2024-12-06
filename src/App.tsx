ed7b141901dc514f40e7ecaa4502ae1db7948bc6
Justin Fernald <jfernald@my.gcu.edu>
2024-12-06 15:06:18 -0700

    add contribution day info and color changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,12 @@ import { ContributionQuery } from './api/query';
 import './App.css';
 import { Skyline } from './components/skyline';
 
+import tunnel from 'tunnel-rat';
+export const t = tunnel();
+
 export default function App() {
-  const [name, setName] = useState("Battlesquid");
-  const [year, setYear] = useState(new Date().getFullYear())
+  const [name, setName] = useState('Battlesquid');
+  const [year, setYear] = useState(new Date().getFullYear());
 
   const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
   const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure();
@@ -18,8 +21,8 @@ export default function App() {
     variables: {
       name,
       start: `${year}-01-01T00:00:00Z`,
-      end: `${year}-12-31T00:00:00Z`
-    }
+      end: `${year}-12-31T00:00:00Z`,
+    },
   });
 
   const input = useRef<HTMLInputElement>(null!);
@@ -29,26 +32,29 @@ export default function App() {
   return (
     <AppShell
       header={{ height: 60 }}
-      padding={"md"}
+      padding={'md'}
       navbar={{
         width: 300,
         breakpoint: 'sm',
         collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
-      }}>
+      }}
+    >
       <AppShell.Header>
         <Group h="100%" px="md">
           <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
           <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
           skyline
           <TextInput ref={input} placeholder="Github Username" />
-          <Button onClick={() => setName(input.current.value.trim())} >Search</Button>
+          <Button onClick={() => setName(input.current.value.trim())}>Search</Button>
         </Group>
       </AppShell.Header>
-      <AppShell.Main style={{ height: "calc(100vh - 96px)", backgroundColor: theme.colors.dark[7] }}>
-        <LoadingOverlay visible={result.fetching} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} />
+      <AppShell.Main style={{ height: 'calc(100vh - 96px)', backgroundColor: theme.colors.dark[7] }}>
+        <LoadingOverlay visible={result.fetching} zIndex={1000} overlayProps={{ radius: 'sm', blur: 2 }} />
         {!result.fetching && <Skyline weeks={result.data!.user!.contributionsCollection.contributionCalendar.weeks} />}
+        <div style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, pointerEvents: 'none' }}>
+          <t.Out />
+        </div>
       </AppShell.Main>
     </AppShell>
-  )
+  );
 }
-
--- src/components/contribution_tower.tsx
+++ src/components/contribution_tower.tsx
@@ -1,44 +1,88 @@
-import { MeshProps } from "@react-three/fiber";
-import { useRef, useState } from "react";
-import * as THREE from "three";
+import { MeshProps } from '@react-three/fiber';
+import { useRef, useState } from 'react';
+import * as THREE from 'three';
+
+import { t } from '../App';
+import { Card, Text } from '@mantine/core';
+import { ResultOf } from 'gql.tada';
+import { ContributionQuery } from '../api/query';
 
 interface ContributionTowerProps extends MeshProps {
   x: number;
   y: number;
   height: number;
+  day: NonNullable<
+    ResultOf<typeof ContributionQuery>['user']
+  >['contributionsCollection']['contributionCalendar']['weeks'][number]['contributionDays'][number];
 }
 
 export function ContributionTower(props: ContributionTowerProps) {
-  const { x, y, height, ...rest } = props;
+  const { x, y, height, day, ...rest } = props;
   // This reference will give us direct access to the THREE.Mesh object
-  const ref = useRef<THREE.Mesh>(null!)
+  const ref = useRef<THREE.Mesh>(null!);
   // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
   // Rotate mesh every frame, this is outside of React without overhead
   // useFrame((state, delta) => (ref.current.rotation.x += 0.01))
 
-  return (
-    <mesh
-      {...rest}
-      // ref={ref}
-      position={[x, height / 2, y]}
+  const color = new THREE.Color(day.color);
+  const towerColor = hovered ? color.multiplyScalar(1.3) : color;
 
-      onClick={(event) => {
-        event.stopPropagation();
-        console.log(x, y)
-        click(!clicked)
-      }}
-      onPointerOver={(event) => {
-        event.stopPropagation();
-        hover(true);
-      }}
-      onPointerOut={(event) => {
-        event.stopPropagation();
-        hover(false)
-      }}>
-      <boxGeometry args={[1, height, 1]} />
-      <meshStandardMaterial color={hovered ? '#2e6c80' : '#4287f5'} />
-    </mesh>
-  )
+  return (
+    <>
+      <mesh
+        {...rest}
+        // ref={ref}
+        position={[x, height / 2, y]}
+        onClick={(event) => {
+          event.stopPropagation();
+          console.log(x, y);
+          click(!clicked);
+        }}
+        onPointerMove={(event) => {
+          if (!hovered) {
+            return;
+          }
+
+          event.stopPropagation();
+          setPosition({ x: event.clientX, y: event.clientY });
+        }}
+        onPointerOver={(event) => {
+          event.stopPropagation();
+          hover(true);
+        }}
+        onPointerOut={(event) => {
+          event.stopPropagation();
+          hover(false);
+        }}
+      >
+        <boxGeometry args={[1, height, 1]} />
+        {/* <meshStandardMaterial color={hovered ? '#2e6c80' : '#4287f5'} /> */}
+        <meshStandardMaterial color={towerColor} />
+      </mesh>
+      <t.In>
+        {hovered && (
+          <div
+            key={`${x}:${y}`}
+            className="animate-in"
+            style={{
+              position: 'absolute',
+              left: position.x,
+              top: position.y,
+            }}
+          >
+            <Card>
+              <Text fw={500}>{new Date(day.date).toLocaleDateString(undefined, { dateStyle: 'short' })}</Text>
+              <Text size="sm" c="dimmed">
+                Contributions: {day.contributionCount}
+              </Text>
+            </Card>
+          </div>
+        )}
+      </t.In>
+    </>
+  );
 }
