b27db6db8063f7802acddac285da1dc8d463ffed
Battlesquid <25509915+Battlesquid@users.noreply.github.com>
2025-05-18 10:55:46 -0700

    fix: drop react-scan

--- packages/app/rsbuild.config.ts
+++ packages/app/rsbuild.config.ts
@@ -5,14 +5,5 @@ export default defineConfig({
   plugins: [pluginReact()],
   html: {
     title: "skyline",
-    tags: [
-      {
-        tag: "script",
-        attrs: {
-          src: "https://cdn.jsdelivr.net/npm/react-scan/dist/auto.global.js",
-        },
-        append: false,
-      },
-    ],
   }
 });
