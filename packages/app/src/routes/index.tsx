dab991af04994d549b993575fa6530b4c3a36266
Battlesquid <25509915+Battlesquid@users.noreply.github.com>
2025-07-25 16:59:35 -0700

    wip: more login page work

--- packages/app/src/routes/index.tsx
+++ packages/app/src/routes/index.tsx
@@ -1,10 +1,14 @@
-import { Button, Card, Center, Divider, Stack, Tabs, Text, TextInput, Title } from '@mantine/core'
-import { IconPhoto, IconMessageCircle, IconSettings } from '@tabler/icons-react'
+import { ActionIcon, Button, Card, Center, Divider, Group, Stack, TextInput, Title, Image, Flex } from '@mantine/core'
 import { createFileRoute } from '@tanstack/react-router'
+import classes from "../styles/input_button.module.css";
+import { IconArrowRight } from '@tabler/icons-react';
+import { Box } from '@react-three/drei';
 
 export const Route = createFileRoute('/')({
     component: Login,
-})
+});
+
+// todo: mess w/ isometric tower graphics
 
 export function Login() {
     return (
@@ -12,25 +16,38 @@ export function Login() {
             <Stack>
                 <Title order={4}>{import.meta.env.PUBLIC_APP_NAME}</Title>
                 <Card padding="xl">
-                    <Stack>
-                        <Button
-                            component="a"
-                            href={import.meta.env.PUBLIC_WORKER_URL}
-                            fullWidth={true}
-                        >
-                            Login to Github
-                        </Button>
-                        <Divider my="xs" label="OR" labelPosition="center" />
-                        <Button
-                            component="a"
-                            href={import.meta.env.PUBLIC_WORKER_ENTERPRISE_URL}
-                            fullWidth={true}
-                        >
-                            Login to Github (Enterprise)
-                        </Button>
-                        <TextInput
-                        />
-                    </Stack>
+                    <Flex gap={15}>
+                        <Stack justify="center" gap={0}>
+                            <Title>Your Contributions</Title>
+                            <Title>In 3D</Title>
+                        </Stack>
+                        <Stack justify="center">
+                            <Button
+                                component="a"
+                                href={import.meta.env.PUBLIC_WORKER_URL}
+                                fullWidth={true}
+                            >
+                                Login to Github
+                            </Button>
+                            <Button
+                                component="a"
+                                href={import.meta.env.PUBLIC_WORKER_ENTERPRISE_URL}
+                                fullWidth={true}
+                            >
+                                Login to Github (Enterprise)
+                            </Button>
+                            <TextInput
+                                placeholder="Server Enterprise Host"
+                                radius={9}
+                                rightSection={
+                                    <ActionIcon>
+                                        <IconArrowRight stroke={1} />
+                                    </ActionIcon>
+                                }
+                            />
+                        </Stack>
+                    </Flex>
+
                 </Card>
             </Stack>
         </Center>
