f58e65bf9eb4447f91624455cc3582256534cf08
Battlesquid <esanoluwamayowa@gmail.com>
2025-08-18 09:37:37 -0700

    wip: ttf loader work

--- packages/app/src/hooks/useTTFLoader.ts
+++ packages/app/src/hooks/useTTFLoader.ts
@@ -1,15 +1,20 @@
 import opentype from "opentype.js";
 import {suspend} from "suspend-react";
 
-const FONT_CACHE: Record<string, opentype.Font> = {};
+const FONT_CACHE = new Map<string, opentype.Font>();
 
 const loader = async (url: string) => {
+    const cachedFont = FONT_CACHE.get(url);
+    if (cachedFont !== undefined) {
+        return cachedFont;
+    }
     const response = await fetch(url);
     const buffer = await response.arrayBuffer();
     const font = opentype.parse(buffer);
-    FONT_CACHE[url] = font;
+    FONT_CACHE.set(url, font);
+    return font;
 }
 
-export const useTTFLoader = () => {
-    return suspend(loader, [])
+export const useTTFLoader = (url: string) => {
+    return suspend(loader, [url]);
 }
